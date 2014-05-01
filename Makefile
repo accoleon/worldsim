eb539ca32e3575a218fde6a24db6de2068f76d74
Kevin Xu <jxu@uoregon.edu>
2014-05-01 14:03:19 -0700

    Corrected SDL2 linking, incorporated pixel drawing example into worldsim.cpp

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
 CC:=icpc
 CXX:=icpc
 CXXFLAGS:=-O3 -Wall -Wextra -pedantic -std=c++11
-LDFLAGS:=-ltbb -lsdl2 -std=c++11
+LDFLAGS:=-ltbb -lSDL2 -std=c++11
 UNAME:=$(shell uname)
 ifeq ($(UNAME),Darwin)
-#export CPLUS_INCLUDE_PATH=/usr/local/Cellar/gcc/4.8.2/lib/gcc/x86_64-apple-darwin13.1.0/4.8.2/include/c++:/usr/local/Cellar/gcc/4.8.2/lib/gcc/x86_64-apple-darwin13.1.0/4.8.2/include/c++/x86_64-apple-darwin13.1.0
-#CXXFLAGS+=-gxx-name=g++-4.8 -no-use-clang-env
-#LDFLAGS+=-gxx-name=g++-4.8 -no-use-clang-env
 endif
 VPATH=Components Entities Systems
 
--- worldsim.cpp
+++ worldsim.cpp
@@ -28,39 +28,63 @@ void setupGraphics() {
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return;
 	}
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 0, 0, -1, -1,
-		SDL_WINDOW_FULLSCREEN);
-	if (win == nullptr){
+	SDL_Window *window = SDL_CreateWindow("Hello World!", 0, 0, -1, -1,
+		SDL_WINDOW_FULLSCREEN_DESKTOP);
+	if (window == nullptr){
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		return;
 	}
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == nullptr){
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr){
 		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		return ;
 	}
-	SDL_Surface *bmp = SDL_LoadBMP("osnap_logo.bmp");
-	if (bmp == nullptr){
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		return;
-	}
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-	SDL_FreeSurface(bmp);
-	if (tex == nullptr){
-		std::cout << "SDL_CreateTextureFromSurface Error: "
-			<< SDL_GetError() << std::endl;
-		return;
-	}
-	SDL_RenderClear(ren);
-	SDL_RenderCopy(ren, tex, NULL, NULL);
-	SDL_RenderPresent(ren);
+	SDL_DisplayMode current;
+	SDL_GetCurrentDisplayMode(0, &current);
+	cout << "current w: " << current.w << " current h: " << current.h << endl;
+  SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, current.w, current.h);
+  Uint32 * pixels = new Uint32[current.w * current.h];
+  memset(pixels, 255, current.w * current.h * sizeof(Uint32));
+	bool leftMouseButtonDown = false;
+  bool quit = false;
+  SDL_Event event;
+  while (!quit)
+  {
+      SDL_UpdateTexture(texture, NULL, pixels, current.w * sizeof(Uint32));
+      SDL_WaitEvent(&event);
 
-	SDL_Delay(2000);
-	
-	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+      switch (event.type)
+      {
+      case SDL_MOUSEBUTTONUP:
+          if (event.button.button == SDL_BUTTON_LEFT)
+              leftMouseButtonDown = false;
+          break;
+      case SDL_MOUSEBUTTONDOWN:
+          if (event.button.button == SDL_BUTTON_LEFT)
+              leftMouseButtonDown = true;
+      case SDL_MOUSEMOTION:
+          if (leftMouseButtonDown)
+          {
+              int mouseX = event.motion.x;
+              int mouseY = event.motion.y;
+              pixels[mouseY * current.w + mouseX] = 0;
+          }
+          break;
+      case SDL_QUIT:
+          quit = true;
+          break;
+      }
+
+      SDL_RenderClear(renderer);
+      SDL_RenderCopy(renderer, texture, NULL, NULL);
+      SDL_RenderPresent(renderer);
+  }
+
+  delete[] pixels;
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
 void teardownGraphics() {
 
