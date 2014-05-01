a5d33930d122f1e944ae1f08cfd2279f8c43eaa3
Kevin Xu <jxu@uoregon.edu>
2014-04-30 17:14:03 -0700

    Updated Makefile to do conditional compilation on OSX. Set video mode to 0 so resolution is auto-detected (kinda).

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 CC:=icpc
 CXX:=icpc
-CPPFLAGS:=-debug -Wall -Wextra -pedantic -stdlib=libc++
-LDFLAGS:=-lsfml-system -lsfml-graphics -lsfml-window -ltbb -stdlib=libc++
+CPPFLAGS:=-debug -Wall -Wextra -pedantic
+LDFLAGS:=-lsfml-system -lsfml-graphics -lsfml-window -ltbb
+UNAME:=$(shell uname)
+ifeq ($(UNAME),Darwin)
+CPPFLAGS+=-stdlib=libc++
+LDFLAGS+=-stdlib=libc++
+endif
 
 all: worldsim
-	
+
 worldsim: worldsim.o units.o World.o EntityManager.o
 
 testSFML: testSFML.o
--- worldsim.cpp
+++ worldsim.cpp
@@ -22,7 +22,7 @@ units world;
 
 void setupGraphics() {
 	// Setup Window
-	sf::RenderWindow window(sf::VideoMode(200, 200), "Testing");
+	sf::RenderWindow window(sf::VideoMode(0, 0), "Testing", sf::Style::Fullscreen);
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
@@ -31,7 +31,7 @@ void setupGraphics() {
     sf::Event event;
     while (window.pollEvent(event))
     {
-      if (event.type == sf::Event::Closed)
+      if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
         window.close();
     }
 
