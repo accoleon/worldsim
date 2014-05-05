165edbd84c64396649a17a8ab797c24fc54e026f
Kevin Xu <jxu@uoregon.edu>
2014-05-05 11:08:50 -0700

    Mucking around with the graphics code - thinking of reorganizing the overall game loop structure.

--- Systems/RenderSystem.cpp
+++ Systems/RenderSystem.cpp
@@ -4,16 +4,19 @@
 // University of Oregon
 // 2014-05-01
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 using std::cout;
+using std::endl;
 #include <string>
 using std::string;
 #include "RenderSystem.h"
 
 namespace gws {
-	const int width(800);
-	const int height(600);
+	const int width(640);
+	const int height(480);
 	RenderSystem::RenderSystem(World& world, SDL_Window* window) : world(world), window(window) {
+		// Most of the code below should be moved into functions
 		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 		if (renderer == nullptr){
 			std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
@@ -22,15 +25,14 @@ namespace gws {
 	  SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
 	  Uint32 * pixels = new Uint32[width * height];
 	  memset(pixels, 255, width * height * sizeof(Uint32));
-		cout << pixels[0];
 	  bool quit = false;
 	  SDL_Event event;
-
+		auto start = clock();
+		int frameCount = 0;
 	  while (!quit) {
 			while(SDL_PollEvent(&event)) {
         //If the user has Xed out the window
-        if( event.type == SDL_QUIT )
-        {
+        if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)) {
             //Quit the program
             quit = true;
         }
@@ -44,7 +46,11 @@ namespace gws {
       SDL_RenderClear(renderer);
       SDL_RenderCopy(renderer, texture, NULL, NULL);
       SDL_RenderPresent(renderer);
+			++frameCount;
 	  }
+		auto timeElapsed = clock() - start;
+		auto secondsElapsed = timeElapsed / CLOCKS_PER_SEC;
+		cout << frameCount / secondsElapsed << " fps" << endl;
 	}
 	RenderSystem::~RenderSystem() {}
 	void RenderSystem::Update() {}
